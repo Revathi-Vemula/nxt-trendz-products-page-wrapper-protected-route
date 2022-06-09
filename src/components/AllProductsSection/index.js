f46ece05f7c7fa093c0647d366acc9a2ac001e44
Revathi-Vemula <revathivemula709@gmail.com>
2022-06-09 05:43:46 +0000

    Protected ROute and Products API

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,60 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const url = 'https://apis.ccbp.in/products'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+
+    if (response.ok === true) {
+      const data = await response.json()
+      console.log(data)
+      const formattedData = data.products.map(eachProduct => ({
+        title: eachProduct.title,
+        brand: eachProduct.brand,
+        id: eachProduct.id,
+        price: eachProduct.price,
+        rating: eachProduct.rating,
+        imageUrl: eachProduct.image_url,
+      }))
+      this.setState({productsList: formattedData})
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
